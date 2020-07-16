22c8d8e3a91c4ddaeccf1a9b1bb165546d71d348
Angelica Velez <avelez9882@gmail.com>
2020-07-16 13:03:20 -0400

    Solution
    added script for solution to Sunset hills challenge

--- Project/Scripts/Solve.js
+++ Project/Scripts/Solve.js
@@ -1,1 +1,27 @@
-﻿
+﻿<script>
+
+    $("#sunBtn").on("click", function () {
+
+        let bldArray = [3, 2, 4, 7, 6, 9];
+            let tallest = 0;
+            let currentBld = 0;
+            let output = new Array();
+
+            for (let loop = 0; loop < 6; loop++) {
+
+                currentBld = bldArray[loop];
+
+                if (currentBld > tallest) {
+                    output.push(currentBld);
+                    tallest = currentBld
+                }
+
+                $("#sunOut").html(`${output.length} buildings can see the sunset. <br />
+                The buildings that can see the sunset are [${output.toString()}].`)
+
+
+            }
+
+
+        });
+    </script>
