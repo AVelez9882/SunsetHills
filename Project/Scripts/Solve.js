203072d0020770643d35f64205116858c8a79322
Angelica Velez <avelez9882@gmail.com>
2020-08-12 13:32:38 -0400

    Change
    changed wording for program description.
    corrected clear button id so that it functions correcrly.
    updated Code  page to show correct script code

--- Project/Scripts/Solve.js
+++ Project/Scripts/Solve.js
@@ -1,27 +1,33 @@
 ﻿<script>
 
+	$("#sunOut").hide();
+
     $("#sunBtn").on("click", function () {
 
-        let bldArray = [3, 2, 4, 7, 6, 9];
+	    let bldArray = [3, 2, 4, 7, 6, 9];
             let tallest = 0;
             let currentBld = 0;
             let output = new Array();
 
             for (let loop = 0; loop < 6; loop++) {
 
-                currentBld = bldArray[loop];
+	            currentBld = bldArray[loop];
 
                 if (currentBld > tallest) {
-                    output.push(currentBld);
+	                output.push(currentBld);
                     tallest = currentBld
                 }
 
-                $("#sunOut").html(`${output.length} buildings can see the sunset. <br />
-                The buildings that can see the sunset are [${output.toString()}].`)
+                $("#sunOut").html(`${output.length} buildings can see the sunset. <br />The buildings that can see the sunset are [${output.toString()}].`)
 
-
+                $("#sunOut").show();
             }
 
-
     });
-</script>
+
+    $("#clrBtn").on("click", function () {
+	$("#sunOut").hide();
+	});
+
+
+ </script>
